041673628df6b3539cf89230a164bf4d872b4c4f
MaksPro <mkozaev@mail.ru>
2017-09-19 15:23:45 -0700

    correcting errors

--- lib/game.rb
+++ lib/game.rb
@@ -20,6 +20,18 @@ class Game
     return slovo.downcase.encode('UTF-8').split("")
   end
 
+  def ask_next_letter
+    puts "\nВведите следующую букву"
+
+    letter = ""
+
+    while letter == ""
+      letter = STDIN.gets.encode("UTF-8").chomp.downcase
+    end
+
+    next_step(letter)
+  end
+
   def next_step(bukva)
     if @status == -1 || @status == 1
       return
@@ -37,45 +49,31 @@ class Game
 
       good_letters << bukva
 
-      if bukva == "е"
-        good_letters << "ё"
+      case bukva
+        when "е" then good_letters << "ё"
+        when "ё" then good_letters << "е"
+        when "и" then good_letters << "й"
+        when "й" then good_letters << "и"
       end
-
-      if bukva == "ё"
-        good_letters << "е"
-      end
-
-      if bukva == "и"
-        good_letters << "й"
-      end
-
-      if bukva == "й"
-        good_letters << "и"
-      end
-
+      
       if (letters - good_letters).empty?
         @status = 1
       end
 
     else
+      case bukva
+        when "е" then bad_letters << "ё"
+        when "ё" then bad_letters << "е"
+        when "и" then bad_letters << "й"
+        when "й" then bad_letters << "и"
+      end
+
       @bad_letters << bukva
       @errors += 1
 
-        if @errors >= 7
-          @status = -1
-        end
+      if @errors >= 7
+        @status = -1
+      end
     end
-  end
-
-  def ask_next_letter
-    puts "\nВведите следующую букву"
-
-    letter = ""
-
-    while letter == ""
-      letter = STDIN.gets.encode("UTF-8").chomp.downcase
-    end
-
-    next_step(letter)
   end
 end
